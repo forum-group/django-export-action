286f5881c0cb8b625b9e83bda2a2bdb07362180b
ivan-fg <70253720+ivan-fg@users.noreply.github.com>
2021-05-24 13:13:39 +0500

    Update introspection.py
    [change] For Django 3

--- export_action/introspection.py
+++ export_action/introspection.py
@@ -5,8 +5,11 @@ from __future__ import unicode_literals, absolute_import
 from itertools import chain
 
 from django.contrib.contenttypes.models import ContentType
-from django.db.models.fields import FieldDoesNotExist
-
+try:
+    from django.db.models.fields import FieldDoesNotExist
+except:
+    from django.core.exceptions import FieldDoesNotExist
+    
 
 def _get_field_by_name(model_class, field_name):
     """
